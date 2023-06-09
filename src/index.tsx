516eb9eeb13e622ce4b3b981f42e01a359c0245f
GangYangZhao <zhaogangyang@ones.ai>
2023-06-09 12:27:13 +0800

    chore: init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { add } from './utils';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>1111</React.StrictMode>
+);
